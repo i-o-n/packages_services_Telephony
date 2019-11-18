a4f44e7a75bc45b52a418b7c4fb7bd959b9cdf45
maxwen <max.weninger@gmail.com>
2019-11-18 23:45:39 +0100

    Telephony: use ION_FINGERPRINT to check for system update
    Change-Id: Idcd7ad36150c1027c4b59a71bef852edfd74d45d
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -478,16 +478,16 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                     SharedPreferences sharedPrefs =
                             PreferenceManager.getDefaultSharedPreferences(mContext);
                     final String lastFingerprint = sharedPrefs.getString(KEY_FINGERPRINT, null);
-                    if (!Build.FINGERPRINT.equals(lastFingerprint)) {
+                    if (!Build.ION_FINGERPRINT.equals(lastFingerprint)) {
                         log(
                                 "Build fingerprint changed. old: "
                                         + lastFingerprint
                                         + " new: "
-                                        + Build.FINGERPRINT);
+                                        + Build.ION_FINGERPRINT);
                         clearCachedConfigForPackage(null);
                         sharedPrefs
                                 .edit()
-                                .putString(KEY_FINGERPRINT, Build.FINGERPRINT)
+                                .putString(KEY_FINGERPRINT, Build.ION_FINGERPRINT)
                                 .apply();
                     }
                     break;
